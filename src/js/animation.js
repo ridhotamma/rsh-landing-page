b63f1cb4fdda8f6dd948f2e96f3dce48f2d3cfe0
Ridho Tamma <ridho.tamma@gmail.com>
2024-09-01 14:43:48 +0700

    add initial animation

---
+++ src/js/animation.js
@@ -0,0 +1,103 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const container = document.querySelector('.animation-container');
+const logo = document.querySelector('.logo');
+
+const slide1 = document.querySelector('.slide__1');
+const letterI = document.querySelector('.letter-i');
+const intro1 = document.querySelector('.slide__intro--1');
+const intro2 = document.querySelector('.slide__intro--2');
+
+let containerRect = container.getBoundingClientRect();
+let logoRect = logo.getBoundingClientRect();
+
+let translateX = logoRect.left - containerRect.left;
+let translateY = logoRect.top - containerRect.top;
+
+window.addEventListener('resize', () => {
+    containerRect = container.getBoundingClientRect();
+    logoRect = logo.getBoundingClientRect();
+
+    translateX = logoRect.left - containerRect.left;
+    translateY = logoRect.top - containerRect.top;
+
+    console.log({ translateX, translateY });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.animation-container',
+            start: 'top top',
+            end: 'bottom top',
+            scrub: true,
+            pin: true,
+        },
+    });
+
+    tl.to(logo, {
+        x: -translateX,
+        y: -translateY,
+        scale: 0.5,
+        duration: 3,
+    })
+        .fromTo(
+            slide1,
+            {
+                y: '60%',
+            },
+            {
+                y: '0%',
+                duration: 3,
+            },
+            '<'
+        )
+        .to(letterI, {
+            scaleY: 3,
+            transformOrigin: 'bottom',
+            duration: 3,
+            delay: 2,
+        })
+        .to(
+            slide1,
+            {
+                y: '10%',
+                duration: 3,
+            },
+            '<'
+        )
+        .fromTo(
+            intro1,
+            {
+                autoAlpha: 0,
+                y: 40,
+            },
+            {
+                y: 0,
+                duration: 3,
+                autoAlpha: 1,
+            }
+        )
+        .to(slide1, { y: '40%', duration: 3 })
+        .to(
+            letterI,
+            {
+                scaleY: 4,
+                transformOrigin: 'bottom',
+                duration: 3,
+            },
+            '<'
+        ).fromTo(
+            intro2,
+            {
+                autoAlpha: 0,
+                y: 40,
+            },
+            {
+                y: 0,
+                duration: 3,
+                autoAlpha: 1,
+            },
+            '<'
+        );
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const menuContainer = document.querySelector('.menu-container');
-  
-    hamburgerMenu.addEventListener('click', function () {
-      hamburgerMenu.classList.toggle('hamburger-menu--active');
-      menuContainer.classList.toggle('menu-container--show');
-    });
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const menuContainer = document.querySelector('.menu-container');
+
+  hamburgerMenu.addEventListener('click', function () {
+    hamburgerMenu.classList.toggle('hamburger-menu--active');
+    menuContainer.classList.toggle('menu-container--show');
   });
-  
+});

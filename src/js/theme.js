1d16faed5ccde9a3bc78a707a64c5cdf595df251
ridhotamma <ridho.tamma@gmail.com>
2024-09-07 22:08:54 +0700

    add theme.js

---
+++ src/js/theme.js
@@ -0,0 +1,16 @@
+const Theme = {
+    blue: {
+        primaryTextColor: '#f5f5f5',
+        primaryBackgroundColor: '#f5f5f5'
+    },
+
+    red: {
+        primaryTextColor: '#f5f5f5',
+        primaryBackgroundColor: '#f5f5f5'
+    },
+
+    yellow: {
+        primaryTextColor: '#f5f5f5',
+        primaryBackgroundColor: '#f5f5f5'
+    }
+}
